97e5d09e813f6a796ab3eb7fc5a40478855697b7
ayezalatif <ayezalatif828@gmail.com>
2023-05-05 23:44:58 +0500

    initilaized files

---
+++ index..js
@@ -0,0 +1,17 @@
+//let software=["ayeza", "ayesha","hina"];
+//push ,unshift and pop
+// blank arry
+ //check position
+//let position=software.indexOf("hina")
+//split
+/*let text="My name is ayeza"
+let wordarry=text.split(" ");*/
+//let textarry=software.join("-");
+/*let software2=["zara","maryan"];
+let newSoftware=software.concat(software2);*/
+ let text=[
+    [ "ayeza", "ayesha" ],
+    ["photoshop", "2months"],
+    ["code", "markup"]
+]
+console.log(text[2][0]);
